7259a1561c95b77fe8c94268b988c3c7fd9ecb38
Matt Rogowski <matt@rogow.ski>
2025-06-09 03:38:57 +0100

    Init

--- app/Nova/Page.php
+++ app/Nova/Page.php
@@ -45,9 +45,9 @@ class Page extends Resource
 
             Text::make('Title'),
 
-            Text::make('Single Content Value', 'content->single_content_value'),
+            Text::make('Single Content Value', 'content->single_value'),
 
-            Repeater::make('Repeater Values', 'content->repeater_values')
+            Repeater::make('Repeater Content Values', 'content->repeater_values')
                 ->repeatables([
                     Repeaters\Values::make(),
                 ])
